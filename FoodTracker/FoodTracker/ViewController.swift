97cf4d0fa923ef24348bc2f2bf7b700b53511881
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-09-27 22:32:36 -0700

    add properties to controller

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var meanNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: Actions
+    
+    
 }
 
